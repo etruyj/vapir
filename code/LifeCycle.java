3d7947988e2a2f754f80ec7e07a673f4d229face
etruyj <etruyj@gmail.com>
2022-01-21 10:57:38 -0500

    trying to clean up file names... urg

---
+++ code/LifeCycle.java
@@ -0,0 +1,12 @@
+package com.socialvagrancy.vail.structures;
+
+public class Lifecycle
+{
+	public String id;
+	public String name;
+	public String modified;
+	public boolean markers;
+	public LifecycleRule[] rules;
+	public String description;
+	public int uploads;
+}
